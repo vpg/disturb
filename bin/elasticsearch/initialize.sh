a2dc3c41cff703dfcf3f50fb47c130aa54719daf
adefretin <adefretin@voyageprive.com>
2017-10-06 13:40:22 +0200

    fix elastic index/alias initialization

--- bin/elasticsearch/initialize.sh
+++ bin/elasticsearch/initialize.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 
-# TODO : Get host & port parameters
-HOST="192.168.100.100"
-PORT="9200"
+HOST="https://elasticsearch5.recette12.vpglabs.site"
+#HOST="192.168.100.100:9200"
 
-
-echo "ELS INIT INDEX"
-echo "--------------"
+echo "------------------------------------------------"
+echo "- DISTURB Elasticsearch context initialization -"
+echo "------------------------------------------------"
 
 INIT_FOLDER_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Elasticsearch indexes to create
-INDEX_LIST=('disturb_context_storage')
+INDEX_LIST=('disturb_context')
 
 for index in $INDEX_LIST; do
 
-    echo "Initializing index $index"
-
-    INDEX_EXISTS_HTTP_CODE="$(curl -sL -w "%{http_code}\\n" $HOST:$PORT/$index -o /dev/null)"
+    INDEX_EXISTS_HTTP_CODE="$(curl -k -sL -w "%{http_code}\\n" $HOST/$index -o /dev/null)"
     if [[ $INDEX_EXISTS_HTTP_CODE == 200 ]]; then
-        echo "'$index' index already exists"
+        echo "-> $index : index already exists"
         continue
     fi
-    #create index with mapping
-    curl -sH "Content-Type: application/json" --data "@$INIT_FOLDER_PATH/init_files/${index}_index.json" $HOST:$PORT/$index -o /dev/null
-    #create alias
-    curl -sH "Content-Type: application/json" --data "@$INIT_FOLDER_PATH/init_files/${index}_alias.json" $HOST:$PORT/_aliases -o /dev/null
 
-    echo "$index index created"
+    #create index with mapping
+    INDEX_HTTP_CODE="$(curl -k -w "%{http_code}\\n" -X PUT -sH "Content-Type: application/json" --data "@$INIT_FOLDER_PATH/init_files/${index}_index.json" $HOST/$index -o /dev/null)"
+    if [[ $INDEX_HTTP_CODE == 200 ]]; then
+        echo "-> $index : index created"
+    else
+       echo "-> $index : Error on create index code $INDEX_HTTP_CODE"
+    fi
+
+    #create alias
+    ALIAS_HTTP_CODE="$(curl -k -w "%{http_code}\\n" -X POST -sH "Content-Type: application/json" --data "@$INIT_FOLDER_PATH/init_files/${index}_alias.json" $HOST/_aliases -o /dev/null)"
+    if [[ $ALIAS_HTTP_CODE == 200 ]]; then
+        echo "-> $index : alias created"
+    else
+       echo "-> $index : Error on create alias $ALIAS_HTTP_CODE"
+    fi
 done;
+echo "------------------------------------------------"
+
+
+
+
+
