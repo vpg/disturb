dbce4f9692c72759731113179ca0e567d06a77b0
Jérôme Bourgeais <jbourgeais@voyageprive.com>
2017-11-13 16:34:01 +0100

    CR + Adding UT

--- Library/Cli/Console.php
+++ Library/Cli/Console.php
@@ -3,7 +3,24 @@ namespace Vpg\Disturb\Cli;
 
 use Phalcon\Cli;
 
+/**
+ * Console
+ * Provides generic func related to PHP CLI
+ *
+ * @package    Disturb
+ * @subpackage Cli
+ */
 class Console extends Cli\Console {
+
+    /**
+     * @const string OPT_KEYS_GROUP_NAME
+     */
+    const OPT_KEYS_GROUP_NAME = 'optKeys';
+
+    /**
+     * @const string OPT_VALS_GROUP_NAME
+     */
+    const OPT_VALS_GROUP_NAME = 'optVals';
 
     /**
      * Parses and validates the given argv according to the worker options config
@@ -15,7 +32,11 @@ class Console extends Cli\Console {
      */
     public static function parseLongOpt(string $argv)
     {
-        preg_match_all('/--(?P<optKeys>\w+)(?:=(?P<optVals>[^ ]*))?/', $argv, $paramMatchHash);
+        preg_match_all(
+            '/--(?P<' . self::OPT_KEYS_GROUP_NAME . '>\w+)(?:=(?P<' . self::OPT_VALS_GROUP_NAME . '>[^ ]*))?/',
+            $argv,
+            $paramMatchHash
+        );
         $paramHash = array_combine(array_values($paramMatchHash['optKeys']), array_values($paramMatchHash['optVals']));
         return $paramHash;
     }
---
+++ Tests/Library/Cli/ConsoleTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Library\Cli;
+
+use Vpg\Disturb\Cli;
+
+
+/**
+ * CLI Console test class
+ *
+ * @author  JEROME BOURGEAIS <jbourgeais@voyageprive.com>
+ */
+class consoleTest extends \Tests\DisturbUnitTestCase
+{
+
+    /**
+     * Test parseLongOpt method
+     *
+     * @return void
+     */
+    public function testParseLongOpt()
+    {
+        $argv = "--foo --bar=gar";
+        $paramHash = Cli\Console::parseLongOpt($argv);
+        $this->assertEquals(
+            [
+                'foo' => '',
+                'bar' => 'gar'
+            ],
+            $paramHash
+        );
+    }
+}
