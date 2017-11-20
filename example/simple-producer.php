ba9c2fd1e6dc706f536fc19e567a109a4cef02d9
Jérôme Bourgeais <jbourgeais@voyageprive.com>
2017-11-20 15:08:16 +0100

    refs #37 - making example simple producer to send multiple messages

--- example/simple-producer.php
+++ example/simple-producer.php
@@ -10,10 +10,17 @@ if (empty($argv[1]) || empty($argv[2])) {
 $msg = $argv[1];
 $topicName = $argv[2];
 $brokers = @$argv[3]?:'localhost';
+$nb = @$argv[4]?:1;
+$prefix = @$argv[5]?:'A';
 
 echo "Sending '$msg' to $brokers:$topicName" . PHP_EOL;
 $kafkaProducer = new RdKafka\Producer();
 $kafkaProducer->addBrokers($brokers);
 $kafkaTopic = $kafkaProducer->newTopic($topicName);
-$kafkaTopic->produce(RD_KAFKA_PARTITION_UA, 0, "$msg");
-echo 'sent' . PHP_EOL;
+$msgHash = json_decode($msg, true);
+for($i=0; $i<$nb; $i++) {
+    $msgHash['payload']['i'] = $prefix.$i;
+    $kafkaTopic->produce(RD_KAFKA_PARTITION_UA, 0, json_encode($msgHash));
+    echo 'sent' . PHP_EOL;
+    sleep(1);
+}
