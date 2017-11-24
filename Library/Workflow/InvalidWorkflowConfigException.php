53d19744855537f4689ba70c171ebb33668c1478
Jérôme Bourgeais <jbourgeais@voyageprive.com>
2017-11-24 14:17:31 +0100

    Adding workflow DTO

---
+++ Library/Workflow/InvalidWorkflowConfigException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Vpg\Disturb\Workflow;
+
+use Vpg\Disturb\Core;
+
+/**
+ * Class InvalidWorkflowConfigException
+ *
+ * @package  Disturb\Core\Config
+ * @author   Jérome BOURGEAIS <jbourgeais@voyageprive.com>
+ * @license  https://github.com/vpg/disturb/blob/master/LICENSE MIT Licence
+ */
+class InvalidWorkflowConfigException extends Core\Exception
+{
+
+}
