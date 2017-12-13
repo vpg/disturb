c2d740fb34b1ccd15b9f72abbd12c741d073ff79
Jérôme Bourgeais <jbourgeais@voyageprive.com>
2017-12-13 11:57:25 +0100

    Adding fully functionnal example using PM2

---
+++ example/Full/BooStep.php
@@ -0,0 +1,25 @@
+<?php
+namespace Vpg\Disturb\Example\Test;
+
+use \Vpg\Disturb;
+use \Vpg\Disturb\Message\MessageDto as Message;
+
+class BooStep implements Disturb\Step\StepServiceInterface
+{
+    public function execute(array $paramHash) : array {
+        $resultHash = [];
+        echo  "Exec". PHP_EOL;
+        for($i=0; $i<rand(1,10); $i++) {
+            echo '.';
+            sleep(1);
+        }
+        echo PHP_EOL . "Done" . PHP_EOL;
+        $resultHash = [
+            'status' => Message::MSG_RETURN_SUCCESS,
+            'data' => [ 'foo' => get_class($this), 'bar' => rand(1,10)]
+        ];
+        return $resultHash;
+    }
+}
+
+
