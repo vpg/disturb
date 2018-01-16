1103d52217b4d5da40f875841e7bd64fdc14bee0
Jérôme Bourgeais <jbourgeais@voyageprive.com>
2018-01-16 10:53:20 +0100

    refs #97 imporving dto coverage

--- Tests/Library/Workflow/WorkflowConfigDtoFactoryTest.php
+++ Tests/Library/Workflow/WorkflowConfigDtoFactoryTest.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace Tests\Library\Core\Storage;
+namespace Tests\Library\Workflow;
 
 use \Vpg\Disturb\Workflow;
+use \Vpg\Disturb\Core\Dto;
 
 
 /**
@@ -35,5 +36,25 @@ class WorkflowConfigDtoFactoryTest extends \Tests\DisturbUnitTestCase
         $workflowConfigDto = Workflow\WorkflowConfigDtoFactory::get(
             realpath(__DIR__ . '/../../Config/InvalidWorkflowConfig-WrongExt.foo')
         );
+    }
+
+    /**
+     * Test wrong file ext
+     *
+     * @return void
+     */
+    public function testDTOInput()
+    {
+        $workflowConfigDto = new Workflow\WorkflowConfigDto(
+            '{"name" : "json", "storage" : {"config":{"host":"foo"}}}'
+        );
+        $this->assertEquals($workflowConfigDto->getWorkflowName(), 'json');
+        $workflowConfigDto = new Workflow\WorkflowConfigDto(
+            realpath(__DIR__ . '/../../Config/serie.json')
+        );
+        $this->assertEquals($workflowConfigDto->getWorkflowName(), 'loadingContract');
+        // invalid input type
+        $this->expectException(Dto\InvalidInputTypeException::class);
+        $workflowConfigDto = new Workflow\WorkflowConfigDto(true);
     }
 }
