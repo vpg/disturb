0db92c36af6755c68fdeea0de882429c710bad95
mbrenguier <mbrenguier@voyageprive.com>
2018-02-14 18:26:06 +0100

    refs #74795 - adding disturb starter

---
+++ Library/Client/DisturbStarter.php
@@ -0,0 +1,44 @@
+<?php
+namespace Vpg\Disturb\Client;
+
+use \Phalcon\Mvc\User\Component;
+use \Vpg\Disturb\Message;
+
+class DisturbStarter extends Component
+{
+    /**
+     * Disturb Starter constructor
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Start workflow by sending a message in related topic
+     *
+     * @param String $workflowId   Workflow id
+     * @param Array  $payloadHash  List of params
+     * @param String $brokers      broker list
+     * @param String $topicName    topic name
+     *
+     */
+    public static function start(string $workflowId, array $payloadHash, string $brokers, string $topicName)
+    {
+        $messageHash = [
+            'id' => $workflowId,
+            'type' => Message\MessageDto::TYPE_WF_CTRL,
+            'action' => 'start',
+            'payload' => $payloadHash
+        ];
+        //send message with givens params
+        $kafkaProducer = new \RdKafka\Producer();
+        $kafkaProducer->addBrokers($brokers);
+        $kafkaTopic = $kafkaProducer->newTopic($topicName);
+        error_log($topicName);
+        error_log(print_r($messageHash,1));
+        $kafkaTopic->produce(RD_KAFKA_PARTITION_UA, 0, json_encode($messageHash));
+    }
+}
