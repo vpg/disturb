4e4a83471d9b8720c824416409f604a059419238
mbrenguier <mbrenguier@voyageprive.com>
2018-02-15 15:36:30 +0100

    add get status command

---
+++ Library/Client/Command.php
@@ -0,0 +1,49 @@
+<?php
+namespace Vpg\Disturb\Client;
+
+use \Phalcon\Mvc\User\Component;
+use \Vpg\Disturb\Message;
+use \Vpg\Disturb\Workflow\WorkflowConfigDtoFactory;
+use Vpg\Disturb\Context;
+
+include realpath(__DIR__ . '/../../bin/configCommand.php');
+
+class Command extends Component
+{
+    /**
+     * Start workflow by sending a message in related topic
+     *
+     * @param String $workflowProcessId Workflow id
+     * @param Array  $payloadHash       List of params
+     * @param String $brokers           broker list
+     * @param String $topicName         topic name
+     *
+     */
+    public static function start(string $workflowProcessId, array $payloadHash, string $brokers, string $topicName)
+    {
+        $messageHash = [
+            'id' => $workflowProcessId,
+            'type' => Message\MessageDto::TYPE_WF_CTRL,
+            'action' => 'start',
+            'payload' => $payloadHash
+        ];
+        //send message with givens params
+        $kafkaProducer = new \RdKafka\Producer();
+        $kafkaProducer->addBrokers($brokers);
+        $kafkaTopic = $kafkaProducer->newTopic($topicName);
+        $kafkaTopic->produce(RD_KAFKA_PARTITION_UA, 0, json_encode($messageHash));
+    }
+
+    /**
+     * Get status for a specified workflow process id
+     *
+     * @param string $workflowProcessId      workflow process id
+     * @param string $workflowConfigFilePath workflow config file path
+     */
+    public static function getStatus(string $workflowProcessId, string $workflowConfigFilePath)
+    {
+        $workflowConfigDto = WorkflowConfigDtoFactory::get($workflowConfigFilePath);
+        $contextStorage = new Context\ContextStorageService($workflowConfigDto);
+        return $contextStorage->get($workflowProcessId)->getWorkflowStatus();
+    }
+}
